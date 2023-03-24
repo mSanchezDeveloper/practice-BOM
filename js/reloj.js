45d2ca0d59487cc301d625c108bbc119fc8fa076
Marcelo <marcelo.s44@gmail.com>
2023-03-23 20:56:55 -0500

    Funcionalidades de tiempo y fecha agregadas

---
+++ js/reloj.js
@@ -0,0 +1,29 @@
+function obtenerFecha() {
+    let fecha = new Date();
+    let semanas = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
+    let meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+
+    let fechaReloj = document.getElementById(`fecha`);
+    fechaReloj.innerHTML = `${semanas[fecha.getDay()]} ${fecha.getDate()} de ${meses[fecha.getMonth()]} del ${fecha.getFullYear()}`;
+
+    //Arreglas los valores de un digito
+    let horas = fecha.getHours();
+    let minutos = fecha.getMinutes();
+    let segundos = fecha.getSeconds();
+    
+    if (horas < 10) {
+        horas = `0${horas}`;
+    }
+    
+    if (minutos < 10) {
+        minutos = `0${minutos}`;
+    }
+
+    if (segundos < 10) {
+        segundos = `0${segundos}`;
+    }
+
+    let horaReloj = document.querySelector('#hora');
+    horaReloj.innerHTML = `${horas}:${minutos}:${segundos}`;
+}
+setInterval(obtenerFecha, 1000)
